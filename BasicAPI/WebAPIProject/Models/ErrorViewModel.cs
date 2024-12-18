d575345b938c9cfcb22e86ed518d450a2e8ee583
HinaJadav <hina212002@gmail.com>
2024-12-19 01:57:19 +0530

    Basic C# Demo

--- BasicAPI/WebAPIProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAPIProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
