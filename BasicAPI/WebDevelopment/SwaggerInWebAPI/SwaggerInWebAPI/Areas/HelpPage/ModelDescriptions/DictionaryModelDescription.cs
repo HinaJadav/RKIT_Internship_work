e5c9d9c1b9b64fab2fdda492771c94c295e0a31f
Hina Jadav <Hina.J@dev.rkitsoft.local>
2025-01-06 11:38:30 +0530

    Swagger use in WebAPI

---
+++ BasicAPI/WebDevelopment/SwaggerInWebAPI/SwaggerInWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SwaggerInWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BasicAPI/WebDevelopment/SwaggerInWebAPI/SwaggerInWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SwaggerInWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
