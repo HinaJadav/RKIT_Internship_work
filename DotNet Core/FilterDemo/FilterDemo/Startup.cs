60601fe445769032410c1b716036fdbd63403814
Hina Jadav <hina212002@gmail.com>
2025-01-28 18:54:56 +0530

    basic filters

---
+++ DotNet Core/FilterDemo/FilterDemo/Startup.cs
@@ -0,0 +1,47 @@
+﻿namespace FilterDemo
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Configures services for Dependency Injection.
+        /// </summary>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // Add Swagger services
+            services.AddSwaggerGen();
+        }
+
+        /// <summary>
+        /// Configures the middleware pipeline.
+        /// </summary>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Enable Swagger middleware
+            app.UseSwagger();
+
+            // Enable Swagger UI middleware
+            app.UseSwaggerUI();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
