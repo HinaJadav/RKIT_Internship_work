fbdb6cc02f788fdfee421551107335a07f80a111
HinaJadav <hina212002@gmail.com>
2025-02-16 02:56:26 +0530

    TextArea Demo modifications

--- DevExtreme/EditorsDemo/EditorsDemo/JS/TextArea.js
+++ DevExtreme/EditorsDemo/EditorsDemo/JS/TextArea.js
@@ -1,5 +1,5 @@
 ﻿$(function () {
-    $("#fullName").dxTextArea({
+    let fullNameInstance = $("#fullName").dxTextArea({
         accessKey: 'N', // alt + Shift + f 
         placeholder: "Full name",
         // activeStateEnabled: it is allows UI component visually responds (changes its state) as a result of user interaction like touch, click, tap (mostly useful for mobile devices)
@@ -8,8 +8,8 @@
         // autoResizeEnabled: it allows the text area automatically expand or shrink based on content inside it
         // it makes 
         autoResizeEnabled: true,
-        minHeight: 40,
-        maxHeight: 200,
+        minHeight: 40, // default parameter = px
+        maxHeight: 100,
         focusStateEnabled: true,
         hint: "Enter student's full name.",
         hoverStateEnabled: true,
@@ -23,39 +23,67 @@
         spellcheck: true,
         stylingMode: "Outlined",
         tabIndex: 1,
-        width: 750,
-        // event
-        /*onChange: function(e) {
+        width: 500,
+        maxLength: 50,
+        // event 
+        onValueChanged: function (e) {
+            let remaining = 50 - e.value.length;
+            $("#charCount").text(`Remaining Characters: ${remaining}`);
+        }
+    }).dxTextArea("instance");
 
-        }*/
-    });
-    $("#feedback").dxTextArea({
+    let feedackInstance = $("#feedBack").dxTextArea({
         accessKey: 'F', // alt + Shift + f 
         placeholder: "Enter your full name",
         // autoResizeEnabled: it allows the text area automatically expand or shrink based on content inside it
         // it makes dynamically adjust text area size
-
+        width: 500,
         autoResizeEnabled: true,
         minHeight: 40,
-        maxHeight: 200,
+        maxHeight: 100, 
+        maxLength: 500,
+        tabIndex: 2,
+        inputAttr: { maxLength: 500 },
         focusStateEnabled: true,
-        hoverStateEnabled: true
+        hoverStateEnabled: true,
+        onValueChanged: function (e) {
+            let wordCount = e.value.trim().split(/\s+/).filter(Boolean).length;
+            $("#wordCount").text("Word Count: " + wordCount);
+        }
 
-    });
+    }).dxTextArea("instance");
 
-    //$("<div id='wordCount'>Word Count: 0</div>").insertAfter("#feedback");
-
+    
     $("#reset").dxButton({
         text: "Reset",
         onClick: function () {
-            let fullName = $("#fullName").dxTextArea("instance");
+            ("#fullName").dxTextArea("instance").option("value", "");
+            ("#feedBack").dxTextArea("instance").option("value", "");
+            ("#charCount").text("Remaining Character : 50");
+            ("#wordCount").text("Word Count: 0");
+        }
+    });
 
-            fullName.blur(); // remove focus and make it blur
-            fullName.option("readOnly", true); // disable the text are withput chnaging its value
+    $("#submit").dxButton({
+        text: "Submit",
+        onClick: function () {
+            let fullName = $("#fullName").dxTextArea("instance").option("value").trim();
+            let feedBack = $("#feedBack").dxTextArea("instance").option("value").trim();
 
-            // Reset all text areas
-            $("#fullName").dxTextArea("instance").option("value", "");
-            $("#feedback").dxTextArea("instance").option("value", "");
+            if (fullName.length === 0) {
+                alert("Please! Enter your Full name.");
+                fullNameInstance.focus();
+                return;
+            }
+            if (feedBack.length < 20) {
+                alert("Please! Enter proper feedback in detail.");
+                feedackInstance.focus();
+                return;
+            }
+
+            alert("Your feedback form submitted successfully.");
+            fullNameInstance.reset();
+            feedackInstance.reset();
         }
     })
 });
@@ -77,6 +105,14 @@
 // onValueChanged: triggers when value of component is changed it gives real-time updates
     // mostly use for: validation, auto-save, enabling-disabling buttons
 // onChange: triggers when input loses focus
-    // mostly use for: final validation, form submission
+// mostly use for: final validation, form submission
+
+// What Happens when basic Event function runs?
+// ex: I fire "onValueChanged" event on dxTextArea :
+// first pass : "e" parameter which contains :
+// e.value : new user input text
+// e.preivousValue : old input value before new input 
+// e.component : instance of user-type (for ex here is "dxTextArea")
+// e.event : the event that triggered the change
 
 
