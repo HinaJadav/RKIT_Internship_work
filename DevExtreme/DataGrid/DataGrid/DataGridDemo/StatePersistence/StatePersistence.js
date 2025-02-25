96a593618c6f016f02c143deede255d637196d9b
Hina Jadav <hina212002@gmail.com>
2025-02-25 18:54:12 +0530

    State Persistence

---
+++ DevExtreme/DataGrid/DataGrid/DataGridDemo/StatePersistence/StatePersistence.js
@@ -0,0 +1,33 @@
+﻿import { users, countries, states } from '../userData.js';
+
+$(function () {
+    $("#dataGrid").dxDataGrid({
+        dataSource: users,
+        columnFixing: { enabled: true },
+        allowColumnReordering: true,
+        columnAutoWidth: true,
+        rowAlternationEnabled: true,
+        showBorders: true,
+
+        editing: {
+            mode: "row",
+            allowUpdating: true,
+            allowAdding: true,
+            allowDeleting: true,
+        },
+
+        stateStoring: {
+            enabled: true,
+            type: "custom",
+            customLoad: function () {
+                return sendStorageRequest("storageKey", "json", "GET");
+            },
+            customSave: function (state) {
+                sendStorageRequest("storageKey", "text", "PUT", state);
+            }
+        },
+       
+    });
+
+    
+});
